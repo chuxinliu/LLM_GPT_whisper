325bba4c928d68cbdd1964334e98019470e6df52
Chuxin Liu <36140861+chuxinliu@users.noreply.github.com>
2024-08-11 18:53:22 -0400

    Update openai_manager.py

--- src/openai_manager.py
+++ src/openai_manager.py
@@ -1,22 +1,27 @@
 import openai
-from langchain.llms import OpenAI 
+client = openai.OpenAI()
 
 class OpenAIManager:
     """Manages interactions with OpenAI's Whisper and GPT-4 models."""
     
     def __init__(self, api_key=""):
         openai.api_key = api_key
-        self.llm = OpenAI(temperature=0.2)
     
-    def transcribe_audio(self, audio_file, model="whisper-1", prompt="金融, 中英混杂"):
-        transcript = openai.Audio.transcribe(
-            model=model,
-            file=audio_file,
-            response_format="text",
-            prompt=prompt
+    def transcribe_audio(self, audio_file):
+        ''' SOTA Whisper-1 Model: transcribe audio to text"""
+        transcript = client.audio.transcriptions.create(
+            model='whisper-1',
+            file=audio_file
         )
-        return transcript
+        return transcript.text
     
     def generate_text(self, messages):
-        completion = self.llm.generate(messages)
-        return completion['choices'][0]['message']['content']
+        """Generate text using the standard completion mode."""
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": "你是一个出色和严谨的新闻稿记者。"},
+                {"role": "user", "content": f"{messages}"}
+            ]
+        )
+        return completion.choices[0].message.content
